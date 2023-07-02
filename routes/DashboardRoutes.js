abbe4c87844fde88a782f7b6e9e0227a0e70addf
Giorgi Jikia <giorgiijikia@gmail.com>
2023-07-02 19:00:40 +0400

    IMP: Add navigation links

--- routes/DashboardRoutes.js
+++ routes/DashboardRoutes.js
@@ -30,10 +30,45 @@ export const DashboardMenu = [
 		icon: 'home',
 		link: '/'
 	},
+	// {
+	// 	id: uuid(),
+	// 	title: 'LAYOUTS & PAGES',
+	// 	grouptitle: true
+	// },
 	{
 		id: uuid(),
-		title: 'LAYOUTS & PAGES',
-		grouptitle: true
+		title: 'SMS',
+		icon: 'message-square',
+		children: [
+			{ id: uuid(), link: '/pages/profile', name: 'Messages' },
+			{ id: uuid(), link: '/pages/settings', name: 'Messages Archive'},
+			{ id: uuid(), link: '/pages/billing', name: 'Queue' },
+			{ id: uuid(), link: '/pages/pricing', name: 'Inbox'},
+			{ id: uuid(), link: '/pages/pricing', name: 'Blocked Numbers' }
+		]
+	},	
+	{
+		id: uuid(),
+		title: 'Contacts',
+		icon: 'phone',
+		children: [
+			{ id: uuid(), link: '/pages/profile', name: 'Address Book' },
+			{ id: uuid(), link: '/pages/settings', name: 'Lists'},
+		]
+	},
+
+	{
+		id: uuid(),
+		title: 'Settings',
+		icon: 'settings',
+		children: [
+			{ id: uuid(), link: '/pages/profile', name: 'API Tokens' },
+			{ id: uuid(), link: '/pages/billing', name: 'Numbers'},
+			{ id: uuid(), link: '/pages/pricing', name: 'Triggers'},
+			{ id: uuid(), link: '/components/modal', name: 'Retail Price list'},
+			{ id: uuid(), link: '/pages/settings', name: 'Profile'},
+			{ id: uuid(), link: '/components/cards', name: 'System Messages'},
+		]
 	},
 	{
 		id: uuid(),
@@ -47,114 +82,108 @@ export const DashboardMenu = [
 			{ id: uuid(), link: '/404', name: '404 Error' }
 		]
 	},	
-	{
-		id: uuid(),
-		title: 'Authentication',
-		icon: 'lock',
-		children: [
-			{ id: uuid(), link: '/authentication/sign-in', name: 'Sign In' },
-			{ id: uuid(), link: '/authentication/sign-up', name: 'Sign Up' },
-			{ id: uuid(), link: '/authentication/forget-password', name: 'Forget Password'}			
-		]
-	},
-	{
-		id: uuid(),
-		title: 'Layouts',
-		icon: 'layout',
-		link: '/layouts/layout-vertical'
-	},	
-	{
-		id: uuid(),
-		title: 'UI COMPONENTS',
-		grouptitle: true
-	},	
-	{
-		id: uuid(),
-		title: 'Components',
-		icon: 'monitor',
-		children: [
-			{ id: uuid(), link: '/components/accordions', name: 'Accordions' },
-			{ id: uuid(), link: '/components/alerts', name: 'Alerts' },
-			{ id: uuid(), link: '/components/badges', name: 'Badges' },
-			{ id: uuid(), link: '/components/breadcrumbs', name: 'Breadcrumbs' },
-			{ id: uuid(), link: '/components/buttons', name: 'Buttons' },
-			{ id: uuid(), link: '/components/button-group', name: 'ButtonGroup' },
-			{ id: uuid(), link: '/components/cards', name: 'Cards' },
-			{ id: uuid(), link: '/components/carousels', name: 'Carousel' },
-			{ id: uuid(), link: '/components/close-button', name: 'Close Button' },
-			{ id: uuid(), link: '/components/collapse', name: 'Collapse' },
-			{ id: uuid(), link: '/components/dropdowns', name: 'Dropdowns' },
-			{ id: uuid(), link: '/components/list-group', name: 'Listgroup' },
-			{ id: uuid(), link: '/components/modal', name: 'Modal' },
-			{ id: uuid(), link: '/components/navs', name: 'Navs' },
-			{ id: uuid(), link: '/components/navbar', name: 'Navbar' },
-			{ id: uuid(), link: '/components/offcanvas', name: 'Offcanvas' },
-			{ id: uuid(), link: '/components/overlays', name: 'Overlays' },
-			{ id: uuid(), link: '/components/pagination', name: 'Pagination' },
-			{ id: uuid(), link: '/components/popovers', name: 'Popovers' },
-			{ id: uuid(), link: '/components/progress', name: 'Progress' },
-			{ id: uuid(), link: '/components/spinners', name: 'Spinners' },
-			{ id: uuid(), link: '/components/tables', name: 'Tables' },
-			{ id: uuid(), link: '/components/toasts', name: 'Toasts' },
-			{ id: uuid(), link: '/components/tooltips', name: 'Tooltips' }
-		]
-	},	
-	{
-		id: uuid(),
-		title: 'Menu Level',
-		icon: 'corner-left-down',
-		children: [
-			{ 
-				id: uuid(), 
-				link: '#', 
-				title: 'Two Level',
-				children: [
-					{ id: uuid(), link: '#', name: 'NavItem 1'},
-					{ id: uuid(), link: '#', name: 'NavItem 2' }
-				]
-			},
-			{ 
-				id: uuid(), 
-				link: '#', 
-				title: 'Three Level',
-				children: [
-					{ 
-						id: uuid(), 
-						link: '#', 
-						title: 'NavItem 1',
-						children: [
-							{ id: uuid(), link: '#', name: 'NavChildItem 1'},
-							{ id: uuid(), link: '#', name: 'NavChildItem 2'}
-						]
-					},
-					{ id: uuid(), link: '#', name: 'NavItem 2' }
-				]
-			}
-		]
-	},	
-	{
-		id: uuid(),
-		title: 'Documentation',
-		grouptitle: true
-	},
-	{
-		id: uuid(),
-		title: 'Docs',
-		icon: 'clipboard',
-		link: '/documentation'
-	},
-	{
-		id: uuid(),
-		title: 'Changelog',
-		icon: 'git-pull-request',
-		link: '/changelog'
-	},
-	{
-		id: uuid(),
-		title: 'Download',
-		icon: 'download',
-		link: 'https://codescandy.gumroad.com/l/dashui-nextjs'
-	}
+	// {
+	// 	id: uuid(),
+	// 	title: 'Authentication',
+	// 	icon: 'lock',
+	// 	children: [
+	// 		{ id: uuid(), link: '/authentication/sign-in', name: 'Sign In' },
+	// 		{ id: uuid(), link: '/authentication/sign-up', name: 'Sign Up' },
+	// 		{ id: uuid(), link: '/authentication/forget-password', name: 'Forget Password'}			
+	// 	]
+	// },
+	// {
+	// 	id: uuid(),
+	// 	title: 'UI COMPONENTS',
+	// 	grouptitle: true
+	// },	
+	// {
+	// 	id: uuid(),
+	// 	title: 'Components',
+	// 	icon: 'monitor',
+	// 	children: [
+	// 		{ id: uuid(), link: '/components/accordions', name: 'Accordions' },
+	// 		{ id: uuid(), link: '/components/alerts', name: 'Alerts' },
+	// 		{ id: uuid(), link: '/components/badges', name: 'Badges' },
+	// 		{ id: uuid(), link: '/components/breadcrumbs', name: 'Breadcrumbs' },
+	// 		{ id: uuid(), link: '/components/buttons', name: 'Buttons' },
+	// 		{ id: uuid(), link: '/components/button-group', name: 'ButtonGroup' },
+	// 		{ id: uuid(), link: '/components/cards', name: 'Cards' },
+	// 		{ id: uuid(), link: '/components/carousels', name: 'Carousel' },
+	// 		{ id: uuid(), link: '/components/close-button', name: 'Close Button' },
+	// 		{ id: uuid(), link: '/components/collapse', name: 'Collapse' },
+	// 		{ id: uuid(), link: '/components/dropdowns', name: 'Dropdowns' },
+	// 		{ id: uuid(), link: '/components/list-group', name: 'Listgroup' },
+	// 		{ id: uuid(), link: '/components/modal', name: 'Modal' },
+	// 		{ id: uuid(), link: '/components/navs', name: 'Navs' },
+	// 		{ id: uuid(), link: '/components/navbar', name: 'Navbar' },
+	// 		{ id: uuid(), link: '/components/offcanvas', name: 'Offcanvas' },
+	// 		{ id: uuid(), link: '/components/overlays', name: 'Overlays' },
+	// 		{ id: uuid(), link: '/components/pagination', name: 'Pagination' },
+	// 		{ id: uuid(), link: '/components/popovers', name: 'Popovers' },
+	// 		{ id: uuid(), link: '/components/progress', name: 'Progress' },
+	// 		{ id: uuid(), link: '/components/spinners', name: 'Spinners' },
+	// 		{ id: uuid(), link: '/components/tables', name: 'Tables' },
+	// 		{ id: uuid(), link: '/components/toasts', name: 'Toasts' },
+	// 		{ id: uuid(), link: '/components/tooltips', name: 'Tooltips' }
+	// 	]
+	// },	
+	// {
+	// 	id: uuid(),
+	// 	title: 'Menu Level',
+	// 	icon: 'corner-left-down',
+	// 	children: [
+	// 		{ 
+	// 			id: uuid(), 
+	// 			link: '#', 
+	// 			title: 'Two Level',
+	// 			children: [
+	// 				{ id: uuid(), link: '#', name: 'NavItem 1'},
+	// 				{ id: uuid(), link: '#', name: 'NavItem 2' }
+	// 			]
+	// 		},
+	// 		{ 
+	// 			id: uuid(), 
+	// 			link: '#', 
+	// 			title: 'Three Level',
+	// 			children: [
+	// 				{ 
+	// 					id: uuid(), 
+	// 					link: '#', 
+	// 					title: 'NavItem 1',
+	// 					children: [
+	// 						{ id: uuid(), link: '#', name: 'NavChildItem 1'},
+	// 						{ id: uuid(), link: '#', name: 'NavChildItem 2'}
+	// 					]
+	// 				},
+	// 				{ id: uuid(), link: '#', name: 'NavItem 2' }
+	// 			]
+	// 		}
+	// 	]
+	// },	
+	// {
+	// 	id: uuid(),
+	// 	title: 'Documentation',
+	// 	grouptitle: true
+	// },
+	// {
+	// 	id: uuid(),
+	// 	title: 'Docs',
+	// 	icon: 'clipboard',
+	// 	link: '/documentation'
+	// },
+	// {
+	// 	id: uuid(),
+	// 	title: 'Changelog',
+	// 	icon: 'git-pull-request',
+	// 	link: '/changelog'
+	// },
+	// {
+	// 	id: uuid(),
+	// 	title: 'Download',
+	// 	icon: 'download',
+	// 	link: 'https://codescandy.gumroad.com/l/dashui-nextjs'
+	// }
 ];
 
 export default DashboardMenu;

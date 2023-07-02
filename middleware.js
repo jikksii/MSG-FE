716120ecbdde1999161143c969dc5db0f58e8c67
Giorgi Jikia <giorgiijikia@gmail.com>
2023-07-02 19:52:57 +0400

    FIX: Auth middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,24 @@
 import { NextResponse } from 'next/server'
 export function middleware(request) {
   const userToken = request.cookies.get('your-key')?.value;
-  if(false) {
-     return NextResponse.redirect(new URL('/',request.url))
+
+
+  if (!userToken && request.nextUrl.pathname !== '/authentication/sign-in') {
+    return NextResponse.redirect(new URL('/authentication/sign-in', request.url))
+  }
+
+  if (userToken && request.nextUrl.pathname === '/authentication/sign-in') {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 }
 export const config = {
-  matcher: '/',
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - authentication (API routes)
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     */
+    '/', '/authentication/sign-in',
+  ],
 }

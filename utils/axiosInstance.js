233e20244b590fe1bcc49c1108f346da7b42f7cf
Giorgi Jikia <giorgiijikia@gmail.com>
2023-11-28 15:34:10 +0400

    IMP: Add env host variable

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1/api',
+    baseURL: process.env.NEXT_PUBLIC_HOST + '/api',
     timeout: 1000,
 })
 export default axiosInstance;

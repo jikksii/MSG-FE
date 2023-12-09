f8457302b830808ddfd2e29e277c51aa22f8da20
Giorgi Jikia <giorgiijikia@gmail.com>
2023-12-09 15:12:25 +0400

    IMP: Redirect to login page when unauthorized

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -12,4 +12,19 @@ const token = getCookie('token');
 if (token) {
     axiosInstance.defaults.headers.Authorization = 'Bearer ' + token;
 }
+// Add a response interceptor
+axiosInstance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+
+    if (error.response?.status === 401) {
+        deleteCookie('token');
+        router.push('/authentication/sign-in');
+    }
+    return Promise.reject(error);
+});
 export default axiosInstance;
